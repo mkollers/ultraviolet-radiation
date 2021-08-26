0eb8bf07e91c5049f90248d2ea1f1130322ad31a
Markus Kollers <mail@markuskollers.de>
2021-08-26 10:52:34 +0200

    Fix unit test

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,9 +1,21 @@
+import { AuthClientConfig } from '@auth0/auth0-angular';
+import { Config } from 'src/config';
+import { Mock } from 'src/test';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let authClientConfig: Mock<AuthClientConfig>;
+  let config: Mock<Config>;
+
+  beforeEach(() => {
+    authClientConfig = new Mock<AuthClientConfig>();
+    config = new Mock<Config>();
+  });
+
   it('should create the app', () => {
     // Act
-    const component = new AppComponent();
+    const component = new AppComponent(authClientConfig.instance, config.instance);
 
     // Assert
     expect(component).toBeTruthy();
